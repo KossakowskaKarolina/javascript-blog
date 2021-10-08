bb4fba72a25b3a3b1241062759ab3b4fd9b10328
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2021-10-08 21:34:18 +0200

    Add Handlebar template for tag cloud links

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ document.getElementById('test-button').addEventListener('click', function(){
     articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
     tagLink: Handlebars.compile(document.querySelector('#template-tag-link').innerHTML),
     authorLink: Handlebars.compile(document.querySelector('#template-author-link').innerHTML),
+    tagCloudLink: Handlebars.compile(document.querySelector('#template-tag-cloud-link').innerHTML),
   };
 
   const titleClickHandler = function(event){
@@ -186,18 +187,24 @@ document.getElementById('test-button').addEventListener('click', function(){
     //console.log('tagsParams:', tagsParams);
 
     /* [DONE] create variable for all links HTML code */
-    let allTagsHTML = '';
+    const allTagsData = {tags: []};
 
     /* [DONE] START LOOP: for each tag in allTags: */
     for(let tag in allTags){
 
       /* [DONE] generate code of a link and add it to allTagsHTML */
-      allTagsHTML += '<li><a href="#tag-' + tag + '" class="' + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + ' </a></li> ';
-    /* [DONE] END LOOP: for each tag in allTags: */
+      //allTagsHTML += '<li><a href="#tag-' + tag + '" class="' + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + ' </a></li> ';
+      allTagsData.tags.push({
+        tag: tag,
+        //count: allTags[tag],
+        className: calculateTagClass(allTags[tag], tagsParams)
+      });
+
+      /* [DONE] END LOOP: for each tag in allTags: */
     }
 
     /*[DONE] add HTML from allTagsHTML to tagList */
-    tagList.innerHTML = allTagsHTML;
+    tagList.innerHTML = templates.tagCloudLink(allTagsData);
   };
 
   generateTags();
